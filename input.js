28bc67b038b579b1f948010be31c3198ff37667f
Denny <Dennyng1989@hotmail.com>
2023-02-26 14:51:56 -0800

    Implement quick chat keys and input verification

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { connect } = require("http2");
+
 const setupInput = (conn) => {
   connection = conn;
   const stdin = process.stdin;
@@ -13,15 +15,21 @@ const handleUserInput = function(key) {
   if (key === '\u0003') process.exit();
 
   // store direction string in object
-  const directions = {
-    '\u0077': 'Move: up',
-    '\u0061': 'Move: left',
-    '\u0073': 'Move: down',
-    '\u0064': 'Move: right'
+  const commands = {
+    '\u0077': 'Move: up',     //w key
+    '\u0061': 'Move: left',   //a key
+    '\u0073': 'Move: down',   //s key
+    '\u0064': 'Move: right',  //d key
+    '\u0071': 'Say: Hi!',     //q key
+    '\u0065': 'Say: Bye!',    //e key
+    '\u0072': 'Say: Gotcha!', //r key
   };
 
-  // send directions using key in object
-  connection.write(directions[key]);
+  // only process command if input key is recognized
+  if (Object.keys(commands).includes(key)) {
+    // send directions using key in object
+    connection.write(commands[key]);
+  }
 };
 
 module.exports = { setupInput };
